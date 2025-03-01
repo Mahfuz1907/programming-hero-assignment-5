b1a2be0c600313226e5e6717486e6adc45462ce2
Mahfuz1907 <mahfuztamim1907@gmail.com>
2025-03-02 03:31:47 +0600

    added theme

--- script/script.js
+++ script/script.js
@@ -1,24 +1,3 @@
-document.getElementById('discover').addEventListener("click", 
-    function(){
-        document.getElementById('back').style.display = 'flex';
-        document.getElementById('task').style.display = 'none';
-        document.getElementById('theme').style.display = 'none';
-        document.getElementById('question').style.display = 'block';
-        document.getElementById('main').style.display = 'none';
-    })
-
-
-
-document.getElementById('back').addEventListener("click", 
-    function(){
-        document.getElementById('back').style.display = 'none';
-        document.getElementById('task').style.display = 'flex';
-        document.getElementById('theme').style.display = 'flex';
-        document.getElementById('question').style.display = 'none';
-        document.getElementById('main').style.display = 'grid';
-    })
-
-
 function updateCurrentDate() {
     const weekdayElement = document.getElementById('day');
     const dateElement = document.getElementById('date');
