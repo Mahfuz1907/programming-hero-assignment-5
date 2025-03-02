9d50cef1db1fdffe37151cb46e33c96d680d9735
Mahfuz1907 <mahfuztamim1907@gmail.com>
2025-03-02 19:00:39 +0600

    added bg random

--- script/theme.js
+++ script/theme.js
@@ -1,13 +1,17 @@
 document.getElementById('theme').addEventListener('click', function() {
     function getRandomColor() {
-        const letters = '0123456789ABCDEF';
-        let color = '#';
+        const hexCode = '0123456789ABCDEF';
+        let colorCode = '#';
         for (let i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
+            let number = Math.random();
+            let code = number * 16;
+            let floor = Math.floor(code);
+            colorCode += hexCode[floor];
         }
-        return color;
+        return colorCode;
     }
 
     const randomColor = getRandomColor();
     document.body.style.backgroundColor = randomColor;
 });
+
